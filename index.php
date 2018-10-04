2d34078de69e28bc6c2df5c9f15539c5946ef0ef
tiffany law <ykltiffany@gmail.com>
2018-10-04 16:13:36 +0800

    only tiffany1845 commit master

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ echo "<br><br>Finally DONE !!!!!";
 
 echo "<br><br>GIT PULL repo zepto";
 
-echo "<br><br>GIT PULL by tiffany1845";
+echo "<br><br>GIT PUSH to master... only allow tiffany1845";
 
 ?>
